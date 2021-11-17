4065a591086679ea0985b3555f1472f882de6345
Moses <mosesobike@gmail.com>
2021-11-18 00:14:56 +0100

    fix ui

--- src/Components/Travel.js
+++ src/Components/Travel.js
@@ -3,14 +3,16 @@ import React from "react";
 export default function Travel(props){
     return(
     <div className = " travel-container">
-        <div className="travel-image">
+        <div className = 'travel-sub-container'>
+        <div className= 'travel'>
             <img src ={`/assets/images/${props.coverImg}`} alt = "travel-image"  className = "travel-image"/>
         </div>
         <div>
-            <img src ="/assets/images/location-icon.png"  alt ='Location icon' /><span>{props.location}</span><span><a href>View on Google Maps</a></span>
-            <h2>{props.title}</h2>
-            <p>{props.date}</p>
-            <p>{props.description}</p>
+            <img src ="/assets/images/location-icon.png"  alt ='Location icon' /><span className = "travel-location">{props.location}</span><span className = "travel-link"><a href = "#">View on Google Maps</a></span>
+            <h2 className = "travel-title">{props.title}</h2>
+            <p className ="travel-date"> {props.date}</p>
+            <p className = "travel-description">{props.description}</p>
+        </div>
         </div>
     </div>
     )
